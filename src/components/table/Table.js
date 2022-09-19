b736c30de234e25891803d8e0f192659e551aeed
Pavel <paha_gagarin@mail.ru>
2022-09-19 20:09:53 +0300

    получение эелементов для ресайза

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,19 @@ import { createTable } from "./table.template";
 
 export class Table extends ExcelComponent{
   static className = 'excel__table'
+  constructor($root){
+    super($root,{
+      listeners:['mousedown']
+    })
+  }
   toHTML(){
     return createTable(20)
   }
+
+  onMousedown(e){
+    if(e.target.dataset.resize){
+      console.log('start',e.target.dataset.resize)
+    }
+  }
+
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -13,14 +13,18 @@ function toColumn(col) {
   return `
     <div class="column">
       ${col}
+      <div class="col-resize" data-resize="col"></div>
     </div>
   `;
 }
 
 function createRow(index, content) {
+  const resizer = index ? '<div class="row-resize" data-resize="row"></div>' : '';
   return `
   <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+      <div class="row-info">${index ? index : ''}
+      ${resizer}
+      </div>
       <div class="row-data">${content}</div>
   </div>
   `;
