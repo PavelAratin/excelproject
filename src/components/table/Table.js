1e555866462d37c555baef5a10f5d90de7932c4f
Pavel <paha_gagarin@mail.ru>
2022-09-21 15:43:34 +0300

    добавление утилиты для css

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -19,15 +19,24 @@ export class Table extends ExcelComponent {
       const $parent = $resizer.closest('[data-type="resizable"]')
       const coords = $parent.getCoords()
 
-      const cells =  this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+      const type = $resizer.data.resize;
+      console.log(type)
+
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
 
       document.onmousemove = e => {
-        const delta = Math.floor(e.pageX - coords.right);
-        const value = coords.width + delta
-        $parent.$el.style.width = value + 'px';
-        cells.forEach(el => el.style.width = value +'px')        
+        if (type === 'col') {
+          const delta = Math.floor(e.pageX - coords.right);
+          const value = coords.width + delta
+          $parent.css({width:value +'px'})
+          cells.forEach(el => el.style.width = value + 'px')
+        } else {
+          const delta = e.pageY - coords.bottom;
+          const value = coords.height + delta;
+          $parent.css({height:value +'px'})
+        }
       }
-      document.onmouseup = ()=>{
+      document.onmouseup = () => {
         document.onmousemove = null
       }
     }
--- src/core/dom.js
+++ src/core/dom.js
@@ -39,8 +39,14 @@ class Dom {
   getCoords() {
     return this.$el.getBoundingClientRect()
   }
-  
-  findAll(selector){
+
+  css(styles = {}) {
+    Object.keys(styles).forEach(key =>{
+      this.$el.style[key] = styles[key]
+    })
+  }
+
+  findAll(selector) {
     return this.$el.querySelectorAll(selector)
   }
 }
