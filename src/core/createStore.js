eed9d434233af6e70a911c631f9f7f43c0604163
Pavel <paha_gagarin@mail.ru>
2022-09-26 16:29:59 +0300

    создание store

---
+++ src/core/createStore.js
@@ -0,0 +1,21 @@
+export function createStore(rootReducer,initialState = {}) {
+  let state = rootReducer({...initialState},{type:'__INIT__'});
+  let listeners = []
+  return {
+    subscribe(fn) {
+      listeners.push(fn)
+      return {
+        unsubscribe() {
+          listeners = listeners.filter(listener => listener !== fn)
+        }
+      }
+    },
+    dispatch(action) {
+      state = rootReducer(state,action)
+      listeners.forEach(listener => listener(state))
+    },
+    getState() {
+      return state
+    }
+  }
+}
